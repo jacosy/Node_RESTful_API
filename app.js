b83fa4897c3a9de9f00907022cebefb0aa143ee5
Longoria.You <longoria.you@gmail.com>
2019-10-15 16:20:51 +0800

    wiring up MongoDB

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 /* eslint-disable no-undef */
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const bookRouter = express.Router();
-const port  = process.env.port || 3000;
+const port = process.env.port || 3000;
+
+const mongoUri = 'mongodb+srv://chuyu:p@Ssw0rd@cluster-longo-l4asv.gcp.mongodb.net/bookAPI?retryWrites=true&w=majority';
+const db = mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
-  .get((req, res)=>{
-    const response = {hello: 'This is my API'};
-    res.json(response);
+  .get((req, res) => {
+    Book.find((err, books) => {      
+      if (err === null) {
+        //console.log(books);
+        return res.json(books);
+      }      
+      return res.send(err);
+    });
   });
+
 app.use('/api', bookRouter);
 
-app.get('/', (req, res)=>{
-    res.send('Welcome to my API!');
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
 });
 
-app.listen(port, ()=>{
-    console.log('Running on port: ' + port);
+app.listen(port, () => {
+  console.log('Running on port: ' + port);
 });
---
+++ booksJson.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+db.books.insert([
+    {
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Lev Nikolayevich Tolstoy',
+        read: false
+    },
+    {
+        title: 'Les Miserables',
+        genre: 'Historical Fiction',
+        author: 'Victor Hugo',
+        read: false
+    },
+    {
+        title: 'Secrets of the Javascript Ninja, 2nd Edition',
+        genre: 'Programming',
+        author: 'John Resig, Bear Bibeault & Josip Maras',
+        read: false
+    }
+]);
