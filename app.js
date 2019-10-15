161eebae84790d0fa40fff596b424e5c833105f0
Longoria.You <longoria.you@gmail.com>
2019-10-15 17:16:04 +0800

    Post Data to MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 /* eslint-disable no-undef */
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 const bookRouter = express.Router();
 const port = process.env.port || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoUri = 'mongodb+srv://chuyu:p@Ssw0rd@cluster-longo-l4asv.gcp.mongodb.net/bookAPI?retryWrites=true&w=majority';
 mongoose.connect(mongoUri, {
@@ -14,6 +18,21 @@ mongoose.connect(mongoUri, {
 const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+    book.save((err, book) => {
+      if (err === null) {
+        return res.json({
+          success: true,
+          message: `The newly created book ID is: ${book.id}`
+        });
+      }      
+      return res.json({
+        success: false,
+        message: err
+      });
+    });
+  })
   .get((req, res) => {
     const query = {};
     if (req.query.genre !== undefined) {
@@ -29,8 +48,8 @@ bookRouter.route('/books')
     });
   });
 
-  bookRouter.route('/books/:bookId')
-  .get((req, res) => {  
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
     Book.findById(req.params.bookId, (err, books) => {
       if (err === null) {
         return res.json(books);
