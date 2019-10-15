57c45ff816f221bd32eb4066fadaeaca52c1cb2e
Longoria.You <longoria.you@gmail.com>
2019-10-15 13:18:53 +0800

    First Http Get API

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 const express = require('express');
 
 const app = express();
-
+const bookRouter = express.Router();
 const port  = process.env.port || 3000;
 
+bookRouter.route('/books')
+  .get((req, res)=>{
+    const response = {hello: 'This is my API'};
+    res.json(response);
+  });
+app.use('/api', bookRouter);
+
 app.get('/', (req, res)=>{
-    res.send('Welcome to my first API project!');
+    res.send('Welcome to my API!');
 });
 
 app.listen(port, ()=>{
