822efd471f6bc440f52f6b6862f1de92221c80c5
Longoria.You <longoria.you@gmail.com>
2019-10-17 14:17:27 +0800

    Code Cleanup

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 const app = express();
-const bookRouter = express.Router();
+//const bookRouter = express.Router();
 const port = process.env.port || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,48 +17,6 @@ mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-const Book = require('./models/bookModel');
-
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save((err, book) => {
-      if (err === null) {
-        return res.json({
-          success: true,
-          message: `The newly created book ID is: ${book.id}`
-        });
-      }      
-      return res.json({
-        success: false,
-        message: err
-      });
-    });
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre !== undefined) {
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query, (err, books) => {
-      if (err === null) {
-        //console.log(books);
-        return res.json(books);
-      }
-      return res.send(err);
-    });
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, books) => {
-      if (err === null) {
-        return res.json(books);
-      }
-      return res.send(err);
-    });
-  });
 
 app.use('/api', bookRouter);
 
