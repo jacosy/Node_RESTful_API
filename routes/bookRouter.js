3e21926b15ab36ff5b0aaa1500cc72fcfb115c48
Longoria.You <longoria.you@gmail.com>
2019-10-17 15:40:41 +0800

    Implement PATCH & DELETE API

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -33,6 +33,7 @@ function routes(Book) {
       });
     });
 
+  // Middleware
   bookRouter.use('/books/:bookId', (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err === null) {
@@ -45,6 +46,7 @@ function routes(Book) {
       return res.send(err);
     });
   });
+
   bookRouter.route('/books/:bookId')
     .get((req, res) => res.json(req.book))
     .put((req, res) => {
@@ -53,8 +55,35 @@ function routes(Book) {
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
-      return res.json(book);
+      book.save(err => {
+        if (err === null) {
+          return res.json(book);
+        }
+        return res.status(500).send(err);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id !== undefined) {
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach(item => {
+        book[item[0]] = item[1];
+      });
+      book.save(err => {
+        if (err === null) {
+          return res.json(book);
+        }
+        return res.status(500).send(err);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove(err => {
+        if (err === null) {
+          return res.sendStatus(204);
+        }
+        return res.status(500).send(err);
+      });
     });
 
   return bookRouter;
