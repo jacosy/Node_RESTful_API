fb5145d47b5d3093d2962bf9401fffebfda836a9
Longoria.You <longoria.you@gmail.com>
2019-10-21 15:57:16 +0800

    move logic to controller

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,37 +1,14 @@
 /* eslint-disable no-param-reassign */
 const express = require('express');
+const bookController = require('../controllers/bookController')
 
 function routes(Book) {
   const bookRouter = express.Router();
+  const controller = bookController(Book);
+
   bookRouter.route('/books')
-    .post((req, res) => {
-      const book = new Book(req.body);
-      book.save((err, book) => {
-        if (err === null) {
-          return res.status(201).json({
-            success: true,
-            message: `The newly created book ID is: ${book.id}`
-          });
-        }
-        return res.json({
-          success: false,
-          message: err
-        });
-      });
-    })
-    .get((req, res) => {
-      const query = {};
-      if (req.query.genre !== undefined) {
-        query.genre = req.query.genre;
-      }
-      Book.find(query, (err, books) => {
-        if (err === null) {
-          //console.log(books);
-          return res.json(books);
-        }
-        return res.send(err);
-      });
-    });
+    .post(controller.post)
+    .get(controller.get);
 
   // Middleware
   bookRouter.use('/books/:bookId', (req, res, next) => {
